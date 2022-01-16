cf54ef081ba58be57de911240c3ea6f3ba9e2e81
mohandand <mohandand@gmail.com>
2022-01-16 11:20:43 -0800

    updated code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import { Navbar, NavbarBrand} from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-    Hello
+      <Navbar dark color="primary"> 
+          <div>
+              <NavbarBrand href="/">Mohan Dandigam</NavbarBrand>
+          </div>
+      </Navbar>
     </div>
   );
 }
